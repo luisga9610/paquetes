614feaa700942b895bd81018d2794484dfb3c736
Alumno <Alumno@ws204-03>
2019-02-06 18:00:22 -0800

    te pasas nico te pasas

---
+++ clase1/src/pruebas/Test.java
@@ -0,0 +1,13 @@
+package pruebas;
+
+public class Test {
+
+	public static void main(String[] args) {
+	paquete1.Persona p = new paquete1.Persona(23,"Maria");
+	paquete2.Persona p2 = new paquete2.Persona(24,"Jose","Masculino");
+	System.out.println(p.toString());
+	System.out.println(p2.toString());
+
+	}
+
+}
